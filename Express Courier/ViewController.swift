aa643690113a0e61ec50f2f6ec78c26cc2c590a5
NurilloUmar <nurilloumarov979@gmail.com>
2023-01-04 16:54:19 +0500

    Initial Commit

---
+++ Express Courier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Express Courier
+//
+//  Created by apple on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
