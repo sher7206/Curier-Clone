91f2e2330329ff58a492e82274358604f85fe947
NurilloUmar <nurilloumarov979@gmail.com>
2023-01-05 18:29:34 +0500

    Added Side menu

--- Express Courier/AppDelegate.swift
+++ Express Courier/AppDelegate.swift
@@ -8,7 +8,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        let vc = PostVC(nibName: "PostVC", bundle: nil)
+        let nav = UINavigationController(rootViewController: vc)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- Express Courier/SideMenuVC.swift
+++ Express Courier/SideMenuVC.swift
@@ -9,37 +9,52 @@ import UIKit
 
 class SideMenuVC: UIViewController {
 
-    
-    
     @IBOutlet weak var userBalanceLbl: UILabel!
     @IBOutlet weak var userNameLbl: UILabel!
     @IBOutlet weak var userImage: UIImageView!
-    
-    
     @IBOutlet var contViews: [UIView]!
     @IBOutlet var sideImages: [UIImageView]!
     @IBOutlet var sideNamesLbls: [UILabel]!
-    var sideTexts = ["Pochta","Filliallar","Jamoa","Taksi","Yangiliklar","Dastur xaqida","Dastur xaqida"]
-    var sideImgs = ["box-menu","building-menu","people-menu","car-menu","notification-status-menu","info-circle-menu","24-support-menu"]
-
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var contView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         contViews[0].backgroundColor = UIColor(named: "primary200")
+        contView.transform = .init(translationX: -contView.frame.width, y: 0)
+        blurView.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0.288016414)
+        blurView.alpha = 0
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        for i in 0...sideImgs.count-1{
-            sideImages[i].image = UIImage(named: sideImgs[i])
-            sideNamesLbls[i].text = sideTexts[i]
+        sideMenuAnimation()
+    }
+    
+    func sideMenuAnimation(){
+        navigationController?.navigationBar.isHidden = true
+        UIView.animate(withDuration: 0.3) { [self] in
+            self.contView.transform = .identity
+            blurView.alpha = 1
         }
     }
+
     
     @IBAction func itemBtnPressed(_ sender: UIButton) {
         for i in contViews{
             i.backgroundColor = .white
         }
         contViews[sender.tag].backgroundColor = UIColor(named: "primary200")
+    }
+    
+    @IBAction func dismissBtnPressed(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) { [self] in
+            self.contView.transform = .init(translationX: -self.contView.frame.width, y: 0)
+            blurView.alpha = 0
+        }completion: { _ in
+            self.dismiss(animated: false, completion: nil)
+        }
     }
     
     
