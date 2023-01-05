5671afa782a0138e5321f747e2b5535ebd97672d
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-05 18:29:34 +0500

    Onboarding done!

--- Express Courier/AppDelegate.swift
+++ Express Courier/AppDelegate.swift
@@ -8,7 +8,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        let vc = OnboardingVC()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Express Courier' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Express Courier
+ pod 'SCPageControl'
+
+end
