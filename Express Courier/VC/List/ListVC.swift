d2544259d48f9b9bf6ecf71c7efd8450722d3819
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-13 16:36:39 +0500

    List added

---
+++ Express Courier/VC/List/ListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ListVC.swift
+//  Express Courier
+//
+//  Created by Sherzod on 13/01/23.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigation()
+    }
+    
+    func setupNavigation() {
+        title = "Ro'yhatlar"
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "primary900")
+        appearance.shadowColor = .clear
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationItem.backButtonTitle = ""
+    }
+    
+}
