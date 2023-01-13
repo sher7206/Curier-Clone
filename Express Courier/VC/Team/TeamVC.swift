8b301688993e9527f0c1619d6618dca89512c1ef
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-13 17:14:32 +0500

    ww

--- Express Courier/VC/Team/TeamVC.swift
+++ Express Courier/VC/Team/TeamVC.swift
@@ -56,6 +56,9 @@ extension TeamVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollec
 //MARK: - Funs
 extension TeamVC {
     func setupNavigation() {
+        
+        
+        
         title = "Jamoa"
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
