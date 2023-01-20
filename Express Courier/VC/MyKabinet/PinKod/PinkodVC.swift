05dbc37cdde6753e1ca4f946b0dfe29853b4ccb1
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-20 16:42:49 +0500

    2112

--- Express Courier/VC/MyKabinet/PinKod/PinkodVC.swift
+++ Express Courier/VC/MyKabinet/PinKod/PinkodVC.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class PinkodVC: UIViewController {
     
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var deleteView: UIView!
+    @IBOutlet weak var submitBtnTitle: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
     }
     
     func setupNavigation() {
+        let password = Cache.share.getUserPassword()
         title = "Pin kod"
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
@@ -23,6 +30,25 @@ class PinkodVC: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
         navigationItem.backButtonTitle = ""
+        self.deleteView.isHidden = true
+        self.titleLbl.text = "Siz ilovaga kirish uchun PIN - kod o‘rnatmagansiz "
+        if password != nil {
+            self.deleteView.isHidden = false
+            self.titleLbl.text = "Sizning havfsizligingiz uchun"
+            self.submitBtnTitle.text = "PIN - kodni o'zgartirish"
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let password = Cache.share.getUserPassword()
+        self.deleteView.isHidden = true
+        self.titleLbl.text = "Siz ilovaga kirish uchun PIN - kod o‘rnatmagansiz "
+        if password != nil {
+            self.deleteView.isHidden = false
+            self.titleLbl.text = "Sizning havfsizligingiz uchun"
+            self.submitBtnTitle.text = "PIN - kodni o'zgartirish"
+        }
     }
     
     @IBAction func setupPinTapped(_ sender: UIButton) {
@@ -30,4 +56,8 @@ class PinkodVC: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    @IBAction func deletePinTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        UserDefaults.standard.set(nil, forKey: Keys.userPassword)
+    }
 }
