008811774b28897c29b35ec504b0116e1ff5580f
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-21 19:53:22 +0500

    212

--- Express Courier/Network/Routers/ListRouter.swift
+++ Express Courier/Network/Routers/ListRouter.swift
@@ -39,7 +39,7 @@ enum ListRouter: BaseURLRequestConvertible {
             return .get
         case .statsPackages:
             return .get
-        case .countPackages(let model):
+        case .countPackages:
             return .get
         }
     }
