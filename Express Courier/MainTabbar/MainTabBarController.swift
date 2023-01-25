c4c48e2918d4763b0d8c3ca69d8f1bbf9329c95e
Abdulxamidov-Sherzod <76491309+Abdulxamidov-Sherzod@users.noreply.github.com>
2023-01-25 20:51:21 +0500

    extension added

--- Express Courier/MainTabbar/MainTabBarController.swift
+++ Express Courier/MainTabbar/MainTabBarController.swift
@@ -78,24 +78,5 @@ class MainTabBarController: UITabBarController, UINavigationControllerDelegate, 
         viewControllers = [vc1,vc2,vc3,vc4,vc5]
         
     }
-    
+}
 
-    
-    
-    
-    
-}
-extension UIImage {
-    public func resized(to target: CGSize) -> UIImage {
-        let ratio = min(
-            target.height / size.height, target.width / size.width
-        )
-        let new = CGSize(
-            width: size.width * ratio, height: size.height * ratio
-        )
-        let renderer = UIGraphicsImageRenderer(size: new)
-        return renderer.image { _ in
-            self.draw(in: CGRect(origin: .zero, size: new))
-        }
-    }
-}
